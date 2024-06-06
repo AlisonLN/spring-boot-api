269d1698f4b763c608ceed3d91e14dbb4e518b6d
AlisonLN <alison.nascimento@hotmail.com>
2024-06-06 15:13:36 -0300

    Metodo get por id

--- src/main/java/com/example/sbars/controllers/ProductController.java
+++ src/main/java/com/example/sbars/controllers/ProductController.java
@@ -8,12 +8,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController   // ou rescontroler para implementar um api rest
 public class ProductController {
@@ -27,10 +27,16 @@ public class ProductController {
         BeanUtils.copyProperties(productRecordDto, productModel); // Conversao de dto para productModel
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
-    
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts() {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
-
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id")UUID id) {
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product NOT FOUND. ");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
 }
